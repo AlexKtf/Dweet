7e7ac13357ba91570a60d256db22ee13d97d65c1
AlexKtf <alexandre.ktifa@gmail.com>
2014-07-29 18:11:53 +0200

    VIdeo model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,8 +1,6 @@
 class Video < ActiveRecord::Base
   belongs_to :category
 
-  include YtItem
-
   scope :videos, ->() { where(is_playlist: false) }
   scope :playlists, ->() { where(is_playlist: true) }
 
@@ -12,6 +10,25 @@ class Video < ActiveRecord::Base
 
   def as_json(options = {})
     super(include: [:category])
+  end
+
+  def check_and_prepare
+    begin
+      item = is_playlist ? Yt::Playlist.new(id: url) : Yt::Video.new(id: url)
+      self.title = item.title
+      self.image_preview_url = item.thumbnail_url(:medium)
+
+      if is_playlist
+        # TODO : Calculate playlist duration
+        # self.duration = item.playlist_items.map(:video).map(:duration).reduce(:+)
+        self.first_video_url = item.playlist_items.first.video_id
+      else
+        self.duration = item.duration
+      end
+
+    rescue Yt::Errors::NoItems
+      false        
+    end
   end
 
 end
